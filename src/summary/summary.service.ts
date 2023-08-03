5dd0936173c24c91f2ea323ef608e421028b9a67
Dani <danial@Danis-MacBook-Pro.local>
2023-08-03 03:50:15 +0330

    add logic for summary

--- src/summary/summary.service.ts
+++ src/summary/summary.service.ts
@@ -1,4 +1,27 @@
 import { Injectable } from '@nestjs/common';
+import { ReportType } from 'src/data';
+import { ReportService } from 'src/report/report.service';
 
 @Injectable()
-export class SummaryService {}
+export class SummaryService {
+  constructor(private readonly reportService: ReportService) {}
+  calculateSummaries() {
+    const totalIncomes = this.reportService
+      .getAllReports(ReportType.Income)
+      .reduce((sum, report) => {
+        return sum + report.amount;
+      }, 0);
+
+    const totalExpenses = this.reportService
+      .getAllReports(ReportType.Expense)
+      .reduce((sum, report) => {
+        return sum + report.amount;
+      }, 0);
+
+    return {
+      totalIncome: totalIncomes,
+      totalExpense: totalExpenses,
+      netIncome: totalIncomes - totalExpenses,
+    };
+  }
+}
